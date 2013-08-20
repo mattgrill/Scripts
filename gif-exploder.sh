70e4bc851f1f8e1c5e5c46901cb72c9e4a8d6e8b
Matthew Grill <hello@nuarmy.com>
2013-08-19 19:20:34 -0700

    Updates.

--- gif-exploder.sh
+++ gif-exploder.sh
@@ -1,23 +1,18 @@
 #!/bin/sh
-
-#     .d8888b.  8888888 8888888888      8888888888                   888               888                  
-#    d88P  Y88b   888   888             888                          888               888                  
-#    888    888   888   888             888                          888               888                  
-#    888          888   8888888         8888888    888  888 88888b.  888  .d88b.   .d88888  .d88b.  888d888 
-#    888  88888   888   888             888        `Y8bd8P' 888 "88b 888 d88""88b d88" 888 d8P  Y8b 888P"   
-#    888    888   888   888             888          X88K   888  888 888 888  888 888  888 88888888 888     
-#    Y88b  d88P   888   888             888        .d8""8b. 888 d88P 888 Y88..88P Y88b 888 Y8b.     888     
-#     "Y8888P88 8888888 888             8888888888 888  888 88888P"  888  "Y88P"   "Y88888  "Y8888  888     
-#                                                           888                                             
-#                                                           888                                             
-#                                                           888                                             
-
-# Examples
-# gif-exploder [movie] [scale][not required]
-# gif-exploder plane.avi 50%
-# gif-exploder plane.avi 
-
 set -e
+
+if [ "$1" == "-h" ]; then
+  echo  "     _ ___                _       _         "
+  echo  " ___|_|  _|   ___ _ _ ___| |___ _| |___ ___ "
+  echo  "| . | |  _|  | -_|_'_| . | | . | . | -_|  _|"
+  echo  "|_  |_|_|    |___|_,_|  _|_|___|___|___|_|  "
+  echo  "|___|                |_|                    "
+  echo  " "
+  echo  "./gif-exploder [gif] [output-directory] [scale][not required]"
+  echo  " "           
+  exit 0
+fi
+
 
 # Temp directory
 output=/tmp/gif-exploder
@@ -28,15 +23,19 @@ mkdir $output
 mkdir $output/frames
 frames=/tmp/gif-exploder/frames
 
-if [[ "${2}" == "" ]]
+if [[ "${3}" == "" ]]
 then
   scale="100%"
 else
-  scale=$2
+  scale=$3
 fi
 
 # movie!
 movie=$1
+
+# get the file name                         
+path_explode=(${movie//\// })                                                 
+file_name=${path_explode[${#path_explode[@]} - 1]}
 
 # Convert the movie to a gif
 convert -quiet -scale $scale $movie $output/gif_temp.gif
@@ -45,7 +44,7 @@ convert -quiet -scale $scale $movie $output/gif_temp.gif
 convert $output/gif_temp.gif -coalesce $frames/frame_%03d.gif
 
 # Make a timeline from all the frames and drop it on the desktop
-convert +append $frames/frame_*.gif ~/desktop/gif-exploder.jpg
+convert +append $frames/frame_*.gif $2/$file_name
 
 # Delete the working directory
 rm -rf $output
